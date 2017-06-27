5bb5bc4304f1f8223b78579e269c02bb2e44d67b
yaqili <liliyayaqiqi@sina.com>
2017-06-27 22:45:03 +0800

    test use new lib

--- trunk/web/test.php
+++ trunk/web/test.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <title>新建待办事项</title>
-  <?php include("template/$OJ_TEMPLATE/css_test.php");?>
-  <?php include("template/$OJ_TEMPLATE/js_test.php");?> 
+  <?php require("template/$OJ_TEMPLATE/css_test.php");?>
+  <?php require("template/$OJ_TEMPLATE/js_test.php");?> 
 </head>
 <body>
   <div class="sui-container">
