239f9b86352c4574865df30530ffc795829409a5
Yaqi Li <yaqi.li@ni.com>
2017-06-27 14:23:09 +0800

    fix bug in profile

--- trunk/web/template/nioj/profile.php
+++ trunk/web/template/nioj/profile.php
@@ -16,6 +16,11 @@ header("Pragma: no-cache");
 		if (isset($_SESSION['user_id'])){
 				$sid=$_SESSION['user_id'];
 				$profile.= "<i class=icon-user></i><li><a href=".$path_fix."modifypage.php>$MSG_USERINFO</a></li>&nbsp;<li><a href='".$path_fix."userinfo.php?user=$sid'><span id=red>$sid</span></a></li>";
+			}else{
+				if($OJ_LOGIN_MOD=="hustoj"){
+					$profile.= "<li><a href=".$path_fix."registerpage.php>$MSG_REGISTER</a></li>&nbsp;";
+				}
+			}
 			if (isset($_SESSION['administrator'])||isset($_SESSION['contest_creator'])||isset($_SESSION['problem_editor'])){
            $profile.= "<li><a href=".$path_fix."admin/>$MSG_ADMIN</a></li>&nbsp;";
 			
