cce1984bbb05c96725e8bdee34c031dd3630e6b7
Yaqi Li <yaqi.li@ni.com>
2017-06-27 14:46:25 +0800

    add new string

--- trunk/web/lang/en.php
+++ trunk/web/lang/en.php
@@ -7,6 +7,7 @@
 	$MSG_STATUS="Status";
 	$MSG_RANKLIST="Ranklist";
 	$MSG_CONTEST="Contest";
+	$MSG_EXAM="Interview Questions";
   	$MSG_RECENT_CONTEST="Recent";
 	$MSG_LOGOUT="Logout";
 	$MSG_LOGIN="Login";
