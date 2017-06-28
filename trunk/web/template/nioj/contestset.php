7d701832f1c9d087eb6089c7f8ac8fc90046ffda
Yaqi Li <yaqi.li@ni.com>
2017-06-28 15:28:40 +0800

    contestset style judge

--- trunk/web/template/nioj/contestset.php
+++ trunk/web/template/nioj/contestset.php
@@ -24,18 +24,16 @@
     <div class="sui-container">  
       <!-- Main component for a primary marketing message or call to action -->
       <div class="sui-container">
+        <h3>Onsite Interview <?php echo $_SESSION['user_id'] ?></h3>
         <form method="post" role="form" action=contest.php class="sui-form  form-horizontal">
           <div class="control-group">
-          	<label class="control-label"><?php echo $MSG_SEARCH;?></label>
+            <label class="control-label"><?php echo $MSG_SEARCH;?></label>
             <input name=keyword type=text placeholder="Enter title for search" class="input-xfat">
             <div class="controls">
               <button type="submit" class="sui-btn btn-xlarge btn-primary">Go</button>
             </div>
           </div>
         </form>
-      </div>
-      <div class="sui-container">
-        <h3>Onsite Interview <?php echo $_SESSION['user_id'] ?></h3>
         ServerTime:<span id=nowdate></span>
       </div>
       <table class='sui-table table-zebra table-bordered' width=90%>
