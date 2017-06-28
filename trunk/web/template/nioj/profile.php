357de95c6c2b4d72bd7644277d000e087d3b5d5a
Yaqi Li <yaqi.li@ni.com>
2017-06-28 13:46:37 +0800

    login href

--- trunk/web/template/nioj/profile.php
+++ trunk/web/template/nioj/profile.php
@@ -15,11 +15,11 @@ header("Pragma: no-cache");
 	$profile='';
 		if (isset($_SESSION['user_id'])){
 				$sid=$_SESSION['user_id'];
-				$profile.= "<li role='presentation'><a role='menuitem' href=".$path_fix."logout.php>$MSG_LOGOUT</a></li>&nbsp;";
-				$profile.= "<li role='presentation'><a role='menuitem' href=".$path_fix."registerpage.php>$MSG_REGISTER</a></li>&nbsp;";
+				$profile.= "<li role='presentation'><a role='menuitem' tabindex='-1' href=".$path_fix."logout.php>$MSG_LOGOUT</a></li>&nbsp;";
+				$profile.= "<li role='presentation'><a role='menuitem' tabindex='-1' href=".$path_fix."registerpage.php>$MSG_REGISTER</a></li>&nbsp;";
 		}
 		if (isset($_SESSION['administrator'])){
-           $profile.= "<li><a href=".$path_fix."admin/>$MSG_ADMIN</a></li>&nbsp;";
+           $profile.= "<li role='presentation'><a role='menuitem' tabindex='-1' href=".$path_fix."admin/>$MSG_ADMIN</a></li>&nbsp;";
 		}
 	 //  $profile.="</ul></li>";
 ?>
