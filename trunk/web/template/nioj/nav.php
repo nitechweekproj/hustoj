dfd90659283c38b85d64148d2eb5b1f6d0c6fee3
Yaqi Li <yaqi.li@ni.com>
2017-06-28 09:06:58 +0800

    use sui ui nav

--- trunk/web/template/nioj/nav.php
+++ trunk/web/template/nioj/nav.php
@@ -9,19 +9,11 @@
 	}
 ?>
       <!-- Static navbar -->
-      <nav class="navbar navbar-default" role="navigation" >
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="<?php echo $OJ_HOME?>"><?php echo $OJ_NAME?></a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
+      <div class="sui-navbar navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="sui-container">
+            <a class="sui-brand" href="<?php echo $OJ_HOME?>"><?php echo $OJ_NAME?></a>
+            <ul class="sui-nav">
 	      <?php $ACTIVE="class='active'"?>
             <?php if(isset($_SESSION['administrator'])) {?>
               <li <?php if ($url=="problemset.php") echo " $ACTIVE";?>><a href="<?php echo $path_fix?>problemset.php"><?php echo $MSG_PROBLEMS?></a></li>
@@ -41,18 +33,13 @@
               </a></li>
         <?php }?>
 
+              <li class="sui-dropdown"><a href="javascript:void(0);" data-toggle="dropdown" class="dropdown-toggle"><span id="profile">Login</span><i class="caret"></i></a>
+                <script src="<?php echo $path_fix."template/$OJ_TEMPLATE/profile.php?".rand();?>" ></script>
+              </li>
+
             </ul>
-	    <ul class="nav navbar-nav navbar-right">
-	    <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span id="profile">Login</span><span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-<script src="<?php echo $path_fix."template/$OJ_TEMPLATE/profile.php?".rand();?>" ></script>
-              <!--<li><a href="../navbar-fixed-top/">Fixed top</a></li>-->
-	    </ul>
-	    </li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
+          </div>
+        </div><!--/.nav-collapse -->
+      </div><!--/.container-fluid -->
 
 
