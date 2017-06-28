1fdba05e4501eaf15fc133d23a00a08a54b5ee23
yaqili <liliyayaqiqi@sina.com>
2017-06-28 21:16:54 +0800

    debug

--- trunk/web/template/nioj/index.php
+++ trunk/web/template/nioj/index.php
@@ -45,7 +45,7 @@
         <?php }else{ if(!isset($_SESSION['administrator'])){?>
           <div class="sui-container" style="margin-top:20px">  
             <p class="sui-lead">Check out the interview questions:</p>
-            <?php include("contest.php"); ?>
+            <?php require("contest.php"); ?>
           </div>
         <?php } else{?>
           <div class="sui-container" style="margin-top:20px">  
