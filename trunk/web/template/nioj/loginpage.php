546b7bcd73fdfca4159f74f66caccfd99cd725ac
Yaqi Li <yaqi.li@ni.com>
2017-06-28 10:09:10 +0800

    refactor login page

--- trunk/web/template/nioj/loginpage.php
+++ trunk/web/template/nioj/loginpage.php
@@ -21,28 +21,30 @@
 
   <body>
 
-    <div class="container">
+    <div class="sui-container">
     <?php include("template/$OJ_TEMPLATE/nav.php");?>	    
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
 
-<form action="login.php" method="post" role="form" class="form-horizontal">
-	<div class="form-group">
-	<label class="col-sm-4 control-label"><?php echo $MSG_USER_ID?></label><div class="col-sm-8"><input name="user_id" class="form-control" placeholder="<?php echo $MSG_USER_ID?>" type="text"></div>						</div>
-	<div class="form-group">
-	<label class="col-sm-4 control-label"><?php echo $MSG_PASSWORD?></label><div class="col-sm-8"><input name="password" class="form-control" placeholder="<?php echo $MSG_PASSWORD?>" type="password"></div>						</div>
-<?php if($OJ_VCODE){?>
-
-	<div class="form-group">
-	<label class="col-sm-4 control-label"><?php echo $MSG_VCODE?></label><div class="col-sm-4"><input name="vcode" class="form-control" type="text"></div><div class="col-sm-4"><img alt="click to change" src="vcode.php" onclick="this.src='vcode.php?'+Math.random()" height="30px">*</div>						</div>
-<?php }?>
-	<div class="form-group">
-	<div class="col-sm-offset-4 col-sm-4">
-	<button name="submit" type="submit" class="btn btn-default btn-block"><?php echo $MSG_LOGIN; ?></button>
+<form action="login.php" method="post" role="form" class="sui-form form-horizontal">
+	<div class="control-group">
+    <label class="control-label"><?php echo $MSG_USER_ID?></label>
+    <div class="controls">
+      <input name="user_id" class="form-control" placeholder="<?php echo $MSG_USER_ID?>" type="text">
+    </div>
+  </div>
+	<div class="control-group">
+    <label class="control-label"><?php echo $MSG_PASSWORD?></label>
+    <div class="controls">
+      <input name="password" class="form-control" placeholder="<?php echo $MSG_PASSWORD?>" type="password">
+    </div>
+  </div>
+	<div class="control-group">
+    <label class="control-label"></label>
+	  <button type="submit" class="sui-btn btn-primary"><?php echo $MSG_LOGIN; ?></button>
 	</div>
-	<div class="col-sm-4">
-	<a class="btn btn-default btn-block" href="lostpassword.php"><?php echo $MSG_LOST_PASSWORD; ?></a>
-	</div>
+	<div class="control-group">
+	  <a class="sui-btn" href="lostpassword.php"><?php echo $MSG_LOST_PASSWORD; ?></a>
 	</div>
 </form>					
       </div>
