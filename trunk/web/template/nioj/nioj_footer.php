5dd5115870a41638d28135d3d8ee45720b7c70c9
yaqili <liliyayaqiqi@sina.com>
2017-06-29 22:26:58 +0800

    add footer

--- trunk/web/template/nioj/nioj_footer.php
+++ trunk/web/template/nioj/nioj_footer.php
@@ -1,3 +1,13 @@
+<style>
+	.footer {
+	    border-top: 1px solid #aaa;
+	    padding: 30px;
+	    text-align: center;
+	    color: #666;
+	    font-size: 13px;
+	    margin-top: 50px;
+}
+</style>
 <div class="footer">
 	<ul class="unstyled">
 		<li>All Interview Questions are NI Confidential</li>
