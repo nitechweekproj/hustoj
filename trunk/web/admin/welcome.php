80c4f09a4da58deea95b853395a24520053af481
Yaqi Li <yaqi.li@ni.com>
2017-06-29 13:54:54 +0800

    modify admin welcome page

--- trunk/web/admin/welcome.php
+++ trunk/web/admin/welcome.php
@@ -12,11 +12,9 @@
 </head>
 
 <body>
-   <h1 class="sui-page-header"><i class="sui-icon icon-touch-setting2" style="margin-right:1em;"></i>Welcome, Administrator~</h1>
-   <ul class="unstyled">
-      <li><span class="sui-text-info">You could use the navigation bar displayed left to make use of the system.</span></li>
-      <i class="sui-icon icon-tb-emoji"></i>
-   </ul>
+   <h1 class="sui-page-header"><i class="sui-icon icon-touch-setting2" style="margin-right:1em;"></i>System Administration</h1>
+   <?php require("contest.php"); ?>
+   <?php require("template/$OJ_TEMPLATE/contestset_frame.php"); ?>
 </body>
 </html>
 
